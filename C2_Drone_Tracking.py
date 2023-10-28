a9a2b6ec8c2ad0b5b14e85db84bdf34db15dd08e
Alex Springer <springer.alex.h@gmail.com>
2023-10-28 00:55:49 +0000

    Added noisy track

--- C2_Drone_Tracking.py
+++ C2_Drone_Tracking.py
@@ -9,6 +9,13 @@
             - Commanded rates of climb/descent
             - Commanded headings
         In the current implementation, the attitude dynamics of the vehicle are approximated using ideal equations of motion.
+
+        TODO
+        - Using test_tracker.py as a reference, build test for various filters
+        - Add noise to tracks
+        - Create up to 8 tracks
+        - Output tracks in a form that can be used in the filters
+        - Run it!
 """
 __author__ = "Alex Springer"
 __version__ = "1.0.1"
@@ -22,113 +29,199 @@ from controller.FANGS import GuidanceSystem
 import matplotlib.pyplot as plt
 import tracking.track_generator as track
 
+import sys
+import pytest
+import numpy as np
+import numpy.random as rnd
+from copy import deepcopy
+
+import scipy.stats as stats
+
+import gncpy.filters as gfilts
+import gncpy.dynamics.basic as gdyn
+import gncpy.distributions as gdistrib
+import carbs.swarm_estimator.tracker as tracker
+import serums.models as smodels
+from serums.enums import GSMTypes, SingleObjectDistance
+
 
 def run_C2(stopTime, saveSimulationFilePath=None, saveFiguresFolderPath=None):
     # Define the aircraft (C2) -- Assume it's in a hover
-    C2 = {'lat':36.2434 * utils.d2r,
-          'lon':-112.2822 * utils.d2r,
-          'alt':7000,
-          'roll':0,
-          'pitch':0,
-          'heading':0}
+    C2 = {
+        "lat": 36.2434 * utils.d2r,
+        "lon": -112.2822 * utils.d2r,
+        "alt": 7000,
+        "roll": 0,
+        "pitch": 0,
+        "heading": 0,
+    }
 
-    # Define the aircraft (SR drone)
-    drone_parameters = {'weight_max': 80,
-                        'weight_min': 40,
-                        'speed_max': 75 * utils.knts2fps,
-                        'speed_min': 25 * utils.knts2fps,
-                        'Kf': 0,
-                        'omega_T': 7,
-                        'omega_L': 20,
-                        'omega_mu': 3,
-                        'T_max': 30,
-                        'K_Lmax': 30,
-                        'mu_max': 45 * utils.d2r,
-                        'C_Do': 0.02,
-                        'C_Lalpha': 0.1 / utils.d2r,
-                        'alpha_o': -0.05 * utils.d2r,
-                        'wing_area': 8,
-                        'aspect_ratio': 8,
-                        'wing_eff': 0.8}
+    # Define the aircraft (target drone)
+    drone_parameters = {
+        "weight_max": 80,
+        "weight_min": 40,
+        "speed_max": 75 * utils.knts2fps,
+        "speed_min": 25 * utils.knts2fps,
+        "Kf": 0,
+        "omega_T": 7,
+        "omega_L": 20,
+        "omega_mu": 3,
+        "T_max": 30,
+        "K_Lmax": 30,
+        "mu_max": 45 * utils.d2r,
+        "C_Do": 0.02,
+        "C_Lalpha": 0.1 / utils.d2r,
+        "alpha_o": -0.05 * utils.d2r,
+        "wing_area": 8,
+        "aspect_ratio": 8,
+        "wing_eff": 0.8,
+    }
 
-    # Build the aircraft object
-    with utils.Timer('build_drone_obj'):
+    # Build the drone aircraft object
+    with utils.Timer("build_drone_obj"):
         drone = FixedWingVehicle(drone_parameters, aircraftID=1001)
 
     # Define the drone's initial conditions
-    drone1_init_cond = {'v_BN_W': 50 * utils.knts2fps,
-                        'h': 7000,  # 7000ft MSL
-                        'gamma': 0,  # No ascent or descent
-                        'sigma': 0,  # Northbound
-                        'lat': 36.2434 * utils.d2r,
-                        'lon': -112.2822 * utils.d2r,
-                        'v_WN_N': [10 * utils.knts2fps, 10 * utils.knts2fps, 0],
-                        'weight': 80}
+    drone1_init_cond = {
+        "v_BN_W": 50 * utils.knts2fps,
+        "h": 7000,  # 7000ft MSL
+        "gamma": 0,  # No ascent or descent
+        "sigma": 0,  # Northbound
+        "lat": 36.2434 * utils.d2r,
+        "lon": -112.2822 * utils.d2r,
+        "v_WN_N": [10 * utils.knts2fps, 10 * utils.knts2fps, 0],
+        "weight": 80,
+    }
 
     # Drone PI Guidance Transfer Functions
-    TF_constants = {'K_Tp': 0.15, 'K_Ti': 0.01, 'K_Lp': 0.7, 'K_Li': 0.05, 'K_mu_p': 0.075}
+    TF_constants = {
+        "K_Tp": 0.15,
+        "K_Ti": 0.01,
+        "K_Lp": 0.7,
+        "K_Li": 0.05,
+        "K_mu_p": 0.075,
+    }
 
     # Build the guidance system using the aircraft object and control system transfer function constants
-    with utils.Timer('build_drone_GuidanceSystem_obj'):
+    with utils.Timer("build_drone_GuidanceSystem_obj"):
         drone1_gnc = GuidanceSystem(drone, TF_constants, drone1_init_cond)
-        bearing = [track.target_bearing((C2['lat'], C2['lon'], C2['alt']), (drone1_gnc.lat[-1], drone1_gnc.lon[-1], drone1_gnc.h[-1]), method='lla')]
-    
-    # Initialize the track builder
-    C2_drone1_tracker = track.air_to_air_3d_ideal(C2['lat'], C2['lon'], C2['alt'],
-                                                  C2['roll'], C2['pitch'], C2['heading'],
-                                                  time=drone1_gnc.time[-1])
+        # bearing = [track.target_bearing((C2["lat"], C2["lon"], C2["alt"]), (drone1_gnc.lat[-1], drone1_gnc.lon[-1], drone1_gnc.h[-1]), method="lla")]
 
-    with utils.Timer(f'simulate_{stopTime}_s_drone_flight'):
+    # Initialize the track builder
+    C2_drone1_tracker = track.noisy_a2a(
+        C2["lat"],
+        C2["lon"],
+        C2["alt"],
+        C2["roll"],
+        C2["pitch"],
+        C2["heading"],
+        noise_mean=0,
+        noise_std=1.5,
+        time=drone1_gnc.time[-1])
+    C2_drone1_tracker.track_target(
+        drone1_gnc.lat[-1],
+        drone1_gnc.lon[-1],
+        drone1_gnc.h[-1],
+        drone1_gnc.time[-1])
+
+    with utils.Timer(f"simulate_{stopTime}_s_drone_flight"):
         # drone1_gnc = utils.load_obj(r'saved_simulations\drone1_6min.pkl')
         runSim = True
-        
+
         if runSim:
             while drone1_gnc.time[-1] < stopTime:
-                # "Measure" the drone from the C2 aircraft
-                C2_drone1_tracker.track_target(drone1_gnc.lat[-1], drone1_gnc.lon[-1], drone1_gnc.h[-1], drone1_gnc.time[-1])
 
                 if drone1_gnc.time[-1] >= 1 and drone1_gnc.command.time == 0:
                     # Give the aircraft a command at the ~1 second mark
                     # velocity = 75 knots
                     # rate_of_climb = 5 degrees
                     # heading = 15 degrees (NNE)
-                    drone1_gnc.setCommandTrajectory(75 * utils.knts2fps, 5 * utils.d2r, 15 * utils.d2r)
+                    drone1_gnc.setCommandTrajectory(
+                        75 * utils.knts2fps, 5 * utils.d2r, 15 * utils.d2r
+                    )
                     last_command_time = [drone1_gnc.command.time]
 
-                if drone1_gnc.time[-1] >= 2*60 and drone1_gnc.command.time == last_command_time[0]:
+                if drone1_gnc.time[-1] >= 2 * 60 and drone1_gnc.command.time == last_command_time[0]:
                     # Give the aircraft another command at the 2 minute mark
                     # velocity = 60 mph
                     # rate_of_climb = 0 degrees
                     # heading = -90 degrees (W)
-                    drone1_gnc.setCommandTrajectory(60 * utils.knts2fps, 0 * utils.d2r, -90 * utils.d2r)
+                    drone1_gnc.setCommandTrajectory(
+                        60 * utils.knts2fps, 0 * utils.d2r, -90 * utils.d2r
+                    )
                     last_command_time.append(drone1_gnc.command.time)
 
                 # Generate FANGS output (guidance commands)
                 drone1_gnc.getGuidanceCommands()
 
                 # Calculate aircraft state using ideal EOM
-                new_state = RBFW(drone1_gnc.Vehicle, drone1_gnc.Thrust[-1], drone1_gnc.Lift[-1], drone1_gnc.alpha_c, drone1_gnc.mu[-1], drone1_gnc.h_c, drone1_gnc.v_BN_W[-1], drone1_gnc.gamma[-1], drone1_gnc.sigma[-1], drone1_gnc.mass[-1], drone1_gnc.airspeed[-1], drone1_gnc.lat[-1], drone1_gnc.lon[-1], drone1_gnc.h[-1], drone1_gnc.time[-1], drone1_gnc.dt)
+                new_state = RBFW(
+                    drone1_gnc.Vehicle,
+                    drone1_gnc.Thrust[-1],
+                    drone1_gnc.Lift[-1],
+                    drone1_gnc.alpha_c,
+                    drone1_gnc.mu[-1],
+                    drone1_gnc.h_c,
+                    drone1_gnc.v_BN_W[-1],
+                    drone1_gnc.gamma[-1],
+                    drone1_gnc.sigma[-1],
+                    drone1_gnc.mass[-1],
+                    drone1_gnc.airspeed[-1],
+                    drone1_gnc.lat[-1],
+                    drone1_gnc.lon[-1],
+                    drone1_gnc.h[-1],
+                    drone1_gnc.time[-1],
+                    drone1_gnc.dt)
                 mass, v_BN_W, gamma, sigma, lat, lon, h, airspeed, alpha, drag = new_state
-                drone1_gnc.updateSystemState(mass=mass, v_BN_W=v_BN_W, gamma=gamma, sigma=sigma, lat=lat, lon=lon, h=h, airspeed=airspeed, alpha=alpha, drag=drag)
+                drone1_gnc.updateSystemState(
+                    mass=mass,
+                    v_BN_W=v_BN_W,
+                    gamma=gamma,
+                    sigma=sigma,
+                    lat=lat,
+                    lon=lon,
+                    h=h,
+                    airspeed=airspeed,
+                    alpha=alpha,
+                    drag=drag)
+
+                # "Measure" the drone from the C2 aircraft
+                C2_drone1_tracker.track_target(
+                    drone1_gnc.lat[-1],
+                    drone1_gnc.lon[-1],
+                    drone1_gnc.h[-1],
+                    drone1_gnc.time[-1],
+                )
 
         else:
             for ii in range(len(drone1_gnc.time)):
-                if ii > 0:
-                    bearing.append(track.target_bearing((C2['lat'], C2['lon'], C2['alt']), (drone1_gnc.lat[ii], drone1_gnc.lon[ii], drone1_gnc.h[ii]), method='lla'))
-                C2_drone1_tracker.track_target(drone1_gnc.lat[ii], drone1_gnc.lon[ii], drone1_gnc.h[ii], drone1_gnc.time[ii])
+                C2_drone1_tracker.track_target(
+                    drone1_gnc.lat[ii],
+                    drone1_gnc.lon[ii],
+                    drone1_gnc.h[ii],
+                    drone1_gnc.time[ii],
+                )
 
-    _, (ax_brg, ax_trk) = plt.subplots(2)
-    ax_brg.plot(drone1_gnc.time, C2_drone1_tracker.range)
-    ax_trk.plot(drone1_gnc.time, C2_drone1_tracker.bearing)
-    plt.show()
+    fig, (ax_brg, ax_trk) = plt.subplots(2)
+    ax_brg.plot(drone1_gnc.time, C2_drone1_tracker.target.range)
+    ax_trk.plot(drone1_gnc.time, C2_drone1_tracker.target.bearing)
+    figsavename = r'C:\Users\Alex\Dropbox\UA-MME\2023\Fall\ME-594\Project\figures\\'
+    fig.savefig('track.png')
 
-    utils.plotSim(drone1_gnc, showPlots=True, saveFolder=r'saved_simulations\figures\drone1', filePrefix=f'stopTime')
-    if runSim: utils.save_obj(drone1_gnc, r'saved_simulations\drone1_6min.pkl')
+    # plt.show()
 
-    utils.saveTrack(drone1_gnc, r'saved_simulations\tracks\drone1_6min.csv', downsample=50)
+    # utils.plotSim(drone1_gnc, showPlots=True)
+        # saveFolder=r"saved_simulations\figures\drone1",
+        # filePrefix=f"stopTime")
+    # if runSim:
+    #     utils.save_obj(drone1_gnc, r"saved_simulations\drone1_6min.pkl")
+
+    # utils.saveTrack(
+    #     drone1_gnc, r"saved_simulations\tracks\drone1_6min.csv", downsample=50
+    # )
 
     return
 
 
-if __name__ == '__main__':
-    run_C2(stopTime=6*60)  # 6 minutes
+if __name__ == "__main__":
+    run_C2(stopTime=15)
--- tracking/track_generator.py
+++ tracking/track_generator.py
@@ -3,17 +3,14 @@ import numpy as np
 import tracking.coordinate_transforms as ct
 
 
-class air_to_air_3d_ideal:
+class ideal_a2a:
     def __init__(self, lat, lon, alt, roll, pitch, heading, time=0):
-        # Define the host (measuring) aircraft
-        self.host = self.state(lat, lon, alt, roll, pitch, heading, time)
+        # Define the c2 (measuring) aircraft
+        self.c2 = self.c2_state(lat, lon, alt, roll, pitch, heading, time)
+        self.target = self.track()
+        self.time = [time]
 
-        # Init tracking values
-        self.bearing = []
-        self.lookup = []
-        self.range = []
-
-    class state:
+    class c2_state:
         def __init__(self, lat, lon, alt, roll, pitch, heading, time):
             self.lat = [lat]
             self.lon = [lon]
@@ -21,7 +18,6 @@ class air_to_air_3d_ideal:
             self.roll = [roll]
             self.pitch = [pitch]
             self.heading = [heading]
-            self.time = [time]
 
         def update_state(self, lat, lon, alt, roll, pitch, heading, time):
             self.lat.append(lat)
@@ -30,64 +26,87 @@ class air_to_air_3d_ideal:
             self.roll.append(roll)
             self.pitch.append(pitch)
             self.heading.append(heading)
-            self.time.append(time)
-    
-    def track_target(self, target_lat, target_lon, target_alt, time):
-        brng = target_bearing((self.host.lat[-1], self.host.lon[-1], self.host.alt[-1]),
-                             (target_lat, target_lon, target_alt),
-                             method='lla') - self.host.heading[-1]
-        rng = target_range((self.host.lat[-1], self.host.lon[-1], self.host.alt[-1]),
-                           (target_lat, target_lon, target_alt),
-                           method='lla')
-        self.bearing.append(brng)
-        self.range.append(rng)
-        self.host.time.append(time)
+
+    class track:
+        def __init__(self):
+            # Init tracking values
+            self.bearing = []
+            self.lookup = []
+            self.range = []
+
+    def track_target_ideal(self, target_lat, target_lon, target_alt, dt=0):
+        brng = target_bearing((self.c2.lat[-1], self.c2.lon[-1], self.c2.alt[-1]), (target_lat, target_lon, target_alt), method="lla" ) - self.c2.heading[-1]
+        rng = target_range((self.c2.lat[-1], self.c2.lon[-1], self.c2.alt[-1]), (target_lat, target_lon, target_alt),  method="lla")
+        self.target.bearing.append(brng)
+        self.target.range.append(rng)
+        self.time.append(self.time[-1] + dt)
 
 
-def target_bearing(pos1, pos2=None, angles='Radians', units='feet', method='ned'):
+class noisy_a2a(ideal_a2a):
+    def __init__(
+        self, lat, lon, alt, roll, pitch, heading, noise_mean=0, noise_std=0.01, time=0
+    ):
+        ideal_a2a.__init__(self, lat, lon, alt, roll, pitch, heading, time)
+        self.noise_mean = noise_mean
+        self.noise_std = noise_std
+
+    def track_target(self, target_lat, target_lon, target_alt, dt=0):
+        # TODO Change dt to time, pass time variable
+        # TODO Fix whatever stupid-ass failure is happening with track_target_ideal.
+        self.track_target_ideal(self, target_lat, target_lon, target_alt, dt)
+        self.target.bearing[-1] = self.target.bearing[-1] + np.random(
+            self.noise_mean, self.noise_std
+        )
+
+
+def target_bearing(pos1, pos2=None, angles="Radians", units="feet", method="ned"):
     # TODO: noise/drop-outs
-    if method in ['lla', 'LLA']:
-        if pos2 is None: raise(ValueError('When method is set to lla, pos2 is required.'))
+    if method in ["lla", "LLA"]:
+        if pos2 is None:
+            raise (ValueError("When method is set to lla, pos2 is required."))
         lat1, lon1, alt1, lat2, lon2, alt2, dLon = unpack_lla(pos1, pos2, angles)
         ned = ct.lla_to_NED(lat1, lon1, alt1, lat2, lon2, alt2)
         n1 = ned[0][0]
         e1 = ned[1][0]
         d1 = ned[2][0]
-    elif method in ['ned', 'NED']:
-        if pos2 is not None: print('Reminder: Only pos1 is used when the method is set to NED.')
+    elif method in ["ned", "NED"]:
+        if pos2 is not None:
+            print("Reminder: Only pos1 is used when the method is set to NED.")
         n1, e1, d1 = pos1
-    brng = np.arctan2(e1,n1)
-    if angles in ['Degrees', 'degrees', 'deg', 'Deg', 'd', 'D']:
-        print(f'Converting {brng} Radians to Degrees')
+    brng = np.arctan2(e1, n1)
+    if angles in ["Degrees", "degrees", "deg", "Deg", "d", "D"]:
+        print(f"Converting {brng} Radians to Degrees")
         brng = np.degrees(brng)
     return brng
 
 
-def target_range(pos1, pos2, angles='Radians', units='feet', method='ned'):
+def target_range(pos1, pos2, angles="Radians", units="feet", method="ned"):
     # Convert to NED if needed
-    if method in ['lla', 'LLA']:
-        if pos2 is None: raise(ValueError('When method is set to lla, pos2 is required.'))
+    if method in ["lla", "LLA"]:
+        if pos2 is None:
+            raise (ValueError("When method is set to lla, pos2 is required."))
         lat1, lon1, alt1, lat2, lon2, alt2, dLon = unpack_lla(pos1, pos2, angles)
         ned = ct.lla_to_NED(lat1, lon1, alt1, lat2, lon2, alt2)
         n1 = ned[0][0]
         e1 = ned[1][0]
         d1 = ned[2][0]
-    elif method in ['ned', 'NED']:
-        if pos2 is not None: print('Reminder: Only pos1 is used when the method is set to NED.')
+    elif method in ["ned", "NED"]:
+        if pos2 is not None:
+            print("Reminder: Only pos1 is used when the method is set to NED.")
         n1, e1, d1 = pos1
     # Compute euclidian distance using NED coordinates
     return np.sqrt(n1**2 + e1**2 + d1**2)
 
 
-def target_lookupAngle(pos1, pos2, angles='Radians'):
+def target_lookupAngle(pos1, pos2, angles="Radians"):
     return
 
 
-def unpack_lla(pos1, pos2, angles='Radians'):
+def unpack_lla(pos1, pos2, angles="Radians"):
     lat1, lon1, alt1 = pos1
     lat2, lon2, alt2 = pos2
     dLon = lon2 - lon1
-    if angles in ['Degrees', 'degrees', 'deg', 'Deg', 'd', 'D']:
+    if angles in ["Degrees", "degrees", "deg", "Deg", "d", "D"]:
         lat1 = np.radians(lat1)
         lon1 = np.radians(lon1)
         lat2 = np.radians(lat2)
