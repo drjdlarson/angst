5e9a1613fa5d714c33c9dea7150d630b8335fcd4
Alex Springer <springer.alex.h@gmail.com>
2024-02-09 10:16:07 -0600

    Basic structure of KML writer, need to troubleshoot more

--- sandbox.py
+++ sandbox.py
@@ -1,14 +1,18 @@
 import controller.utils as utils
 import matplotlib.pyplot as plt
+import tracking.kml_writer as kml_writer
 
 
 if __name__ == '__main__':
-    num_drones = 8
-    drones = {}
-    for drone in range(num_drones):
-        print(f'Loading drone {drone+1}...')
-        drone_obj = utils.load_obj(f'./saved_simulations/Grand_Canyon_Search_and_Rescue/abs_command_drone{drone+1}.pkl')
-        drones[str(drone+1)] = drone_obj
-    print(drones.items())
-    utils.plotCoordinates(drones)
-    plt.show()
+    # num_drones = 8
+    # drones = {}
+    # for drone in range(num_drones):
+    #     print(f'Loading drone {drone+1}...')
+    #     drone_obj = utils.load_obj(f'./saved_simulations/Grand_Canyon_Search_and_Rescue/abs_command_drone{drone+1}.pkl')
+    #     drones[str(drone+1)] = drone_obj
+    # print(drones.items())
+    # utils.plotCoordinates(drones)
+    # plt.show()
+
+    obj = utils.load_obj(r'saved_simulations\Grand_Canyon_Search_and_Rescue\abs_command_drone1.pkl')
+    kml_writer.writeKMLfromObj(obj)
--- tracking/kml_writer.py
+++ tracking/kml_writer.py
@@ -2,3 +2,19 @@
 """ Google Earth KML file writer for agent tracks
 """
 
+import simplekml
+import controller.utils as utils
+import tracking.track_generator as track
+
+
+def writeKMLfromObj(GuidanceSystemObject, savePath=None, noise=False, downsample=50):
+    lat = [l * utils.r2d for l in GuidanceSystemObject.lat[::downsample]]
+    lon = [l * utils.r2d for l in GuidanceSystemObject.lon[::downsample]]
+    alt = GuidanceSystemObject.h[::downsample]
+
+    kml = simplekml.Kml()
+    lin = kml.newlinestring(name=f'agent_{GuidanceSystemObject.Vehicle.aircraftID}',
+                            description='Aerial agent track',
+                            coords=[(36.24545584912534,-112.2822),
+                                    (36.2700304603568,-112.30236322830645)])
+    kml.save(f'agent_{GuidanceSystemObject.Vehicle.aircraftID}.kml')
